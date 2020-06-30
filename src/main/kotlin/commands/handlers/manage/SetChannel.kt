d7dc49525735f02ff1bb19c589ef42384dc4014b
LastExceed <chris20194@googlemail.com>
2020-06-30 21:08:04 +0200

    add TODO to missing implementation

--- src/main/kotlin/commands/handlers/manage/SetChannel.kt
+++ src/main/kotlin/commands/handlers/manage/SetChannel.kt
@@ -7,6 +7,6 @@ import org.javacord.api.event.message.*
 
 class SetChannel(processor: CommandProcessor) : CommandHandler(processor) {
 	override fun parseParameterless(event: MessageCreateEvent): CommandResult {
-		return CommandResult.ok { "WIP" }
+		return CommandResult.ok { "WIP" }//TODO: allow configuring channels
 	}
 }
